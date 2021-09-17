755225e73db4e4f28cbdec063bbc60cb4cc28b44
Hbrown16 <hbrown1094@gmail.com>
2021-09-16 21:17:17 -0400

    connection fix 1

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,15 @@
+const sequelize = require('sequelize');
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  se
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+  )
 }
 // const sequelize = process.env.JAWSDB_URL
 //   ? new Sequelize(process.env.JAWSDB_URL)
