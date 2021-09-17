5445e566e5e16a2d9584396d2256ca44679d8415
Hbrown16 <hbrown1094@gmail.com>
2021-09-16 21:44:21 -0400

    prod

--- models/Product.js
+++ models/Product.js
@@ -35,9 +35,15 @@ Product.init(
         isNumeric: true,
       },
     },
-    
-  },
 
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
+  },
   {
     sequelize,
     timestamps: false,
