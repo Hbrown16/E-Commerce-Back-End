7ba0a95c47cd307e40e0f2733ef34781e850bee9
Hbrown16 <hbrown1094@gmail.com>
2021-09-17 17:48:18 -0400

    router.delet

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -137,7 +137,9 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
   Product.destroy({
-    
+    where: {
+      id: req.params.id,
+    },
   })
 });
 
