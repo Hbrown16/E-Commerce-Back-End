c2bd8acc2bf48a0df0dbf2ef934ec33831624f51
Hbrown16 <hbrown1094@gmail.com>
2021-09-16 21:33:36 -0400

    index.js fixproduct

--- models/index.js
+++ models/index.js
@@ -15,12 +15,17 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belondToMany(Tag, {
+Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: "product_id",
 });
 
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: ProductTag;
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
