3b485ee576b9f504443e905aab2b9c910c5bf6a3
Hbrown16 <hbrown1094@gmail.com>
2021-09-16 22:01:56 -0400

    routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,13 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll({
-    
+    attributes: ["id","category_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name","price","stock", "category_id"],
+      },
+    ],
   })
 });
 
