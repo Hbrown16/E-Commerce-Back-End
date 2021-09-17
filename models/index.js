edf16a43ac80974a401d55b17f6ea574a3974f15
Hbrown16 <hbrown1094@gmail.com>
2021-09-16 21:34:20 -0400

    index.js

--- models/index.js
+++ models/index.js
@@ -23,7 +23,7 @@ Product.belongsToMany(Tag, {
 // Tags belongToMany Products (through ProductTag)
 
 Tag.belongsToMany(Product, {
-  through: ProductTag;
+  through: ProductTag,
   foreignKey: "tag_id",
 });
 
