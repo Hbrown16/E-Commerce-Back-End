9fed8cd11e19ed48e04273bffca0b11d2fbf3578
Hbrown16 <hbrown1094@gmail.com>
2021-09-16 21:18:37 -0400

    connection fix 1

--- config/connection.js
+++ config/connection.js
@@ -9,8 +9,14 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
-  )
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
 }
+
 // const sequelize = process.env.JAWSDB_URL
 //   ? new Sequelize(process.env.JAWSDB_URL)
 //   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
