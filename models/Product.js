33d46fc9c10686ef71117b38c6d7ae5c3754ddb4
Hbrown16 <hbrown1094@gmail.com>
2021-09-16 21:42:26 -0400

    prod

--- models/Product.js
+++ models/Product.js
@@ -29,8 +29,13 @@ Product.init(
     },
     stock: {
       type: DataTypes.INTEGER,
-      
-    }
+      allowNull:false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    
   },
 
   {
