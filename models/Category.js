81505a98e253e48dea505bd8845fd4f8444b7ab0
Hbrown16 <hbrown1094@gmail.com>
2021-09-16 21:26:22 -0400

    category fix

--- models/Category.js
+++ models/Category.js
@@ -7,14 +7,24 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
   },
-  {
+  category_name:{
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+},
+{
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'category',
-  }
+}
 );
 
 module.exports = Category;
