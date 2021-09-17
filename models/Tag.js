09b8f51c0cf91086fd756b02b2d2ca2a395703b7
Hbrown16 <hbrown1094@gmail.com>
2021-09-16 21:53:26 -0400

    tag.js

--- models/Tag.js
+++ models/Tag.js
@@ -8,8 +8,14 @@ Tag.init(
   {
     // define columns
     id: {
-      
-    }
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
