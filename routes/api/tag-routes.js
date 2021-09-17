11467fb8b27a5cfd20f8b0fca8bd891d0de2694e
Hbrown16 <hbrown1094@gmail.com>
2021-09-17 18:04:21 -0400

    findall

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,6 +6,9 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    
+  })
 });
 
 router.get('/:id', (req, res) => {
