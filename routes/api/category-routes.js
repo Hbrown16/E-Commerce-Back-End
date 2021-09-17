6bea65674b86f1c34ca0a2cc085ee72a95ba735b
Hbrown16 <hbrown1094@gmail.com>
2021-09-16 22:33:18 -0400

    router.delete start

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -84,6 +84,11 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id:req.params.id,
+    }
+  })
 });
 
 module.exports = router;
