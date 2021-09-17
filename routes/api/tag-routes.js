f80a8f3e4fe2cf9d0cce904fc4c919fa66d67ea5
Hbrown16 <hbrown1094@gmail.com>
2021-09-17 18:05:01 -0400

    findall start

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,7 +7,10 @@ router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   Tag.findAll({
-    
+    attributes: ["id","tag_name"],
+    include: [
+      
+    ]
   })
 });
 
