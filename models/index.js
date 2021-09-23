0ee89f9d83c64c61a9253f5d54c80beb23fa2e36
Hbrown16 <hbrown1094@gmail.com>
2021-09-22 22:32:15 -0400

    debugging index

--- models/index.js
+++ models/index.js
@@ -17,7 +17,9 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
+  through: {
+    model: ProductTag,
+  },
   foreignKey: "product_id",
 });
 
