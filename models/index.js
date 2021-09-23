f11f4f905c4771ff9c3225dbbb29913edf58864a
Hbrown16 <hbrown1094@gmail.com>
2021-09-22 22:30:27 -0400

    debugging con1

--- models/index.js
+++ models/index.js
@@ -1,12 +1,13 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: "category_id",
+  onDelete: "CASCADE",
 });
 
 // Categories have many Products
