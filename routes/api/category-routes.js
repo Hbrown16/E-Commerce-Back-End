549dc6e6cb5dcadf3bc73223e24f6b57676b9114
Hbrown16 <hbrown1094@gmail.com>
2021-09-22 22:52:33 -0400

    debugging cat-r

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,46 +16,32 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  Category.findOne({
-    where: {
-      id: req.params.id,
-    },
-    attributes: ["id", "category_name"],
-    include: [
-      {
-      model: Product,
-      attributes: ["id", "product_name", "price", "stock", "category_id"],
-      },
-    ],
-  })
-  .then((dbCategoryData) => {
-    if (!dbCategoryData) {
-      res.status(404).json({ message: "id not found"});
+  try {
+    const catgor = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!catgor) {
+      res.status(404).json({ message: "No Category with this id" });
       return;
     }
-    res.json(dbCategoryData);
-  })
-  .catch((err) => {
-    console.log(err);
+    res.status(200).json(catgor);
+  } catch (err) {
     res.status(500).json(err);
-  });
+  }
 });
 
 router.post("/", (req, res) => {
   // create a new category
-  Category.create({
-    category_name: req.body.category_name,
-  })
-  .then((dbCategoryData) => res.json(dbCategoryData))
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+  try {
+    const catgor = await Category.create(req.body);
+    res.status(200).json(catgor);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
-
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
   Category.update(req.body, {
