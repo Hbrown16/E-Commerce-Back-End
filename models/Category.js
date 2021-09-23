dc4edba1019bf19a9090f4f39eb4722e986fb7a0
Hbrown16 <hbrown1094@gmail.com>
2021-09-22 22:27:36 -0400

    debugging con

--- models/Category.js
+++ models/Category.js
@@ -8,23 +8,23 @@ Category.init(
   {
     // define columns
     id: {
-      type: DataTypes.INTEGER,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+      category_name:{
+      type: DataTypes.STRING,
       allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
     },
-    category_name:{
-    type: DataTypes.STRING,
-    allowNull: false,
-    },
-},
-{
+  },
+  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: "category",
-}
+  }
 );
 
 module.exports = Category;
