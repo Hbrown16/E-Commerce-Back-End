977fa0796e7cfefe2a95fc79e3a07046545d5144
Hbrown16 <hbrown1094@gmail.com>
2021-09-22 20:51:23 -0400

    debug

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,4 +1,4 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
@@ -31,7 +31,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  Product.findone({
+  Product.findOne({
     where: {
       id: req.params.id,
     },
@@ -73,7 +73,7 @@ router.post('/', (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagId.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -100,7 +100,7 @@ router.put('/:id', (req, res) => {
       id: req.params.id,
     },
   })
-    .then((product) => {
+    .then((_Product) => {
       // find all associated tags from ProductTag
       return ProductTag.findAll({ where: { product_id: req.params.id } });
     })
@@ -148,7 +148,7 @@ router.delete('/:id', (req, res) => {
     }
     res.json(dbProductData);
   })
-  .catch((er) => {
+  .catch((_er) => {
     console.log(err);
     res.status(500).json(err);
   });
