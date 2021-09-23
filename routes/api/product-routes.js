7d5cc6f26d9f1218cef3e3265995c5bb9c45b2e4
Hbrown16 <hbrown1094@gmail.com>
2021-09-22 23:09:10 -0400

    debugging product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -106,7 +106,7 @@ router.put("/:id", (req, res) => {
       id: req.params.id,
     },
   })
-    .then((_Product) => {
+    .then((product) => {
       // find all associated tags from ProductTag
       return ProductTag.findAll({ where: { product_id: req.params.id } });
     })
