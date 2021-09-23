09798b957c9b8df5078ea5f667a2dd731596c226
Hbrown16 <hbrown1094@gmail.com>
2021-09-22 23:14:32 -0400

    debugging tag-route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -64,24 +64,24 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
-  Tag.destroy({
-    where: {
-      id: req.params.id,
-    },
-  })
-  .then((dbTagData) => {
-    if (!dbTagData) {
-      res.status(404).json({ message: "No tag found with id"});
+  try {
+    const alltags = await Tag.destroy({ 
+      where: { 
+        id: req.params.id,
+      },
+    });
+
+    if (!alltags) {
+      res.status(404).json({ message: "No tag found"});
       return;
     }
-    res.json(dbTagData);
-  })
-  .catch((err) => {
-    console.log(err);
+
+    res.status(200).json(alltags);
+  } catch (err) {
     res.status(500).json(err);
-  });
+  }
 });
 
 module.exports = router;
